b9f666fe7a259b1d04aec8fda56fbb663eef3f95
胡杰 <h2syj.zzzz@gmail.com>
2024-06-26 15:06:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from datetime import datetime
+
 from Ai import Ai
 
 ai = Ai()
-print(ai.dialogue("你好"))
+print(f'{datetime.now()} {ai.dialogue("你好")}')
 
 while True:
-    print(ai.dialogue(input(":")))
+    content = input(":")
+    if content == 'exit':
+        break
+    print(f'{datetime.now()} 提问：{content}')
+    reply = ai.dialogue(content)
+    print(f'{datetime.now()} {reply}')
